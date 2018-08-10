c289088b3a9dfed290d5db8e8ac9cbf754612767
Stelios Tsinontas <stelisevil@hotmail.com>
2018-08-10 02:56:08 +0100

    updated todolist-3
    A decent spot to pull request this and ask for help. I've been able to get the remove item function working! Only problem is regardless of which button i click to remove a task, the top task is removed. I've had a look online for any help and general concensus is not to setState twice in a single function. I'm probably making the problem more difficult than it needs to be, but gonna sleep on it for now.

--- src/projects/TodoList/index.js
+++ src/projects/TodoList/index.js
@@ -4,7 +4,9 @@ import style from './style.css';
 class Todo extends React.Component {
   render() {
     return (
-      <p>{this.props.task}</p>
+      <div>
+        <p>{this.props.task} <button onClick={this.props.removeItem}>-</button></p>
+      </div>
     )
   }
 }
@@ -13,15 +15,24 @@ class TodoList extends React.Component {
   constructor() {
     super();
     this.addItem = this.addItem.bind(this)
+    this.removeItem = this.removeItem.bind(this)
     this.state = {
       todos: ['Take out the bins', 'Put a wash load on', 'Learn React'],
-      newItem: ''
+      newItem: '',
+      removedIndex: ''
     }
   }
   addItem() {
     const newTodos = this.state.todos
     newTodos.push(this.state.newItem)
     this.setState({ todos: newTodos, newItem: '' })
+  }
+  removeItem() {
+    let removedItem = this.props.key
+    this.setState({ removedIndex: removedItem })
+    const removedTodos = this.state.todos
+    removedTodos.splice(this.state.todos[this.state.removedIndex], 1)
+    this.setState({ todos: removedTodos, removedIndex: '' })
   }
   render() {
     // Before the return you can do all logic you need
@@ -30,6 +41,7 @@ class TodoList extends React.Component {
         <Todo
           key={i}
           task={task}
+          removeItem={this.removeItem}
         />
       )
     })
