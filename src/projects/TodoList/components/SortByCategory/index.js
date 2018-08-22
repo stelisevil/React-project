fb0269451bd6096bc4e0dd341dbf7858a649612b
Matthew Tsinontas <matthewtsinontas@gmail.com>
2018-08-22 23:06:07 +0100

    PR Comments for you to look at

--- src/projects/TodoList/components/SortByCategory/index.js
+++ src/projects/TodoList/components/SortByCategory/index.js
@@ -3,9 +3,15 @@ import './style.css';
 
 class SortByCategory extends React.Component {
   render () {
-    const todoList = this.props.todoList;
-    const categoriesList = this.props.categoriesList;
+    /*
+      const todoList = this.props.todoList;
+      const categoriesList = this.props.categoriesList;
+    */
+    const { todoList, categoriesList, toggleOrganiseByCategory } = this.props;
 
+    /*
+      foreaches here please
+    */
     let activeCategoryList = [];
     for (let todoIndex = 0; todoIndex < todoList.length; todoIndex++) {
       let eachTodoCategories = todoList[todoIndex].categories;
@@ -21,6 +27,9 @@ class SortByCategory extends React.Component {
       orderByCategoriesObj[activeCategory] = []
     });
 
+    /*
+      foreach
+    */
     for (let todoIndex = 0; todoIndex < todoList.length; todoIndex++) {
       let eachTodoCategories = todoList[todoIndex].categories;
       for (let i = 0; i < eachTodoCategories.length; i++) {
@@ -29,14 +38,34 @@ class SortByCategory extends React.Component {
     }
 
     let displaySortedByCategories = activeCategoryList.map((categoryId, i) => {
+      /*
+        Variable name more descipt, give the find method in-scope category variable a shorter name
+      */
       let eachCategory = categoriesList.find( category => category.id === categoryId )
+
+      /*
+        We can destructue the eachCategory object here to get the values
+        Remember to remove instances of eachCategory. from the names once youve done it
+        const { colour, text, category } = eachCategory;
+      */
+
       let categoryBoxColour = {
         backgroundColor: eachCategory.colour,
+        /*
+          As discussed, this shouldn't be part of the category object because its a
+          value that will never change - if you ever have something in state that will
+          never charge its usually a sign it can be moved out into a render concern.
+
+          E.g. this cateogrytext color, its set as black or white when the colour
+          is created so we can decide the text colour here in render rather than storing it
+        */
         color: eachCategory.text
       };
       let arrayOfCategoryTasks = orderByCategoriesObj[categoryId]
       let listOfCategoryTasks = arrayOfCategoryTasks.map((eachTask, i) => {
+        // Not style, classname
         let styleCompletedTask = (eachTask.completed) ? 'completed' : 'not-completed'
+        // Key should always go on the top element being returned
         return (
           <div className="row">
             <span
@@ -50,6 +79,7 @@ class SortByCategory extends React.Component {
           </div>
         )
       })
+      // ? not sure about this one just double check but have a look at the key
       return (
         <React.Fragment>
           <div key={i} className="row h3 pl-1 m-0" style={categoryBoxColour}>{eachCategory.category}</div>
@@ -63,7 +93,7 @@ class SortByCategory extends React.Component {
           <button
             type="button"
             className="btn btn-primary"
-            onClick={this.props.toggleOrganiseByCategory}
+            onClick={toggleOrganiseByCategory}
           >
             Organise by Category
           </button>
