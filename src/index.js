8537bb6d766bc920142a14755dace72cb8bae05a
Stelios Tsinontas <stelisevil@hotmail.com>
2018-08-29 04:29:37 +0100

    significant progress on data hydration
    I spent a while earlier today styling the program in CSS to look decent, then it was a matter of filling in the places where I could use the data response from the API. I feel it's a good place to check in with you now, as given I don't have access to Twitter's API yet, a lot of what I am doing is simply repeating the same thing over and over. I had a crack at putting links into the tweets and I came up with a solution but as soon as a tweet has multiple links, this stops working. I'd need to see how the API response is for more than one link first before having a crack at the logic. For now though, it works fine.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import IDCard from './projects/IDCard';
 import TodoList from './projects/TodoList';
 import TwitterDashboard from './projects/TwitterDashboard';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
-// ReactDOM.render(<TwitterDashboard />, document.getElementById('root'));
+// ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<TwitterDashboard />, document.getElementById('root'));
 
 registerServiceWorker();
